2ca120c96c036d63e2f8d161dce06cea195e98b4
theda <theda@DANIEL>
2024-10-14 21:28:42 -0700

    Add project files.

---
+++ CalculatorProject/Services/CalculatorService.cs
@@ -0,0 +1,90 @@
+﻿public class CalculatorService
+{
+    private decimal _currentValue;
+    private decimal _previousValue;
+    private string _currentOperation;
+    public string DisplayValue { get; private set; } = "0";
+    private bool isNewEntry = true;
+
+    public void InputDigit(string digit)
+    {
+        if (isNewEntry || DisplayValue == "0")
+        {
+            DisplayValue = digit;
+            isNewEntry = false;
+        }
+        else
+        {
+            DisplayValue += digit;
+        }
+        _currentValue = decimal.Parse(DisplayValue);
+    }
+
+    public void InputDecimal()
+    {
+        if (!DisplayValue.Contains("."))
+        {
+            DisplayValue += ".";
+        }
+    }
+
+    public void SetOperation(string operation)
+    {
+        _previousValue = _currentValue;
+        _currentOperation = operation;
+        isNewEntry = true;
+    }
+
+    public void CalculateResult()
+    {
+        switch (_currentOperation)
+        {
+            case "+":
+                _currentValue = _previousValue + _currentValue;
+                break;
+            case "-":
+                _currentValue = _previousValue - _currentValue;
+                break;
+            case "*":
+                _currentValue = _previousValue * _currentValue;
+                break;
+            case "/":
+                if (_currentValue != 0)
+                {
+                    _currentValue = _previousValue / _currentValue;
+                }
+                else
+                {
+                    DisplayValue = "Error";
+                    return;
+                }
+                break;
+        }
+
+        DisplayValue = _currentValue.ToString();
+        isNewEntry = true;
+    }
+
+    public void Clear()
+    {
+        _currentValue = 0;
+        _previousValue = 0;
+        _currentOperation = null;
+        DisplayValue = "0";
+        isNewEntry = true;
+    }
+
+    public void DeleteLastCharacter()
+    {
+        if (DisplayValue.Length > 1)
+        {
+            DisplayValue = DisplayValue.Substring(0, DisplayValue.Length - 1);
+        }
+        else
+        {
+            DisplayValue = "0";
+        }
+
+        _currentValue = decimal.Parse(DisplayValue);
+    }
+}
